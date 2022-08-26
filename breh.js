210d72c2406e87a415cb9f276a69508d72e782ae
Kevin Radley <kevin.radley@learnet.se>
2022-08-26 15:37:26 +0200

    first commit

---
+++ breh.js
@@ -0,0 +1,49 @@
+/*
+//1.
+console.log("hello wold")
+
+//2.
+console.log("Once upon a time")
+console.log("I was falling in love")
+console.log("Now I’m only falling apart")
+
+//3.
+console.log("Once upon a time \n I was falling in love \n Now I’m only falling apart")
+
+//4.
+var message = "trip and food!";
+console.log(message);
+
+//5.
+var name = "Ada Lovelace";
+// Write your code here:
+console.log(name)
+
+//6.
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let name = prompt("Enter your name: ");
+// Write your code here:
+
+console.log(name)
+
+//7
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let name = prompt("give input: ");
+
+console.log(name)
+console.log(name)
+console.log(name)
+
+//8
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let name = prompt("whats your name ");
+
+console.log("hello") 
+console.log(name)
+*/
